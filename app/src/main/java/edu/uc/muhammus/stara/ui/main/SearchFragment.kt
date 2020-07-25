7e49790f7f953a09bf8fb377f35fee9fe4611ed1
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-07-24 20:33:26 -0400

    Initial iteration of RecyclerView is working in Search Fragment for shows.

--- app/src/main/java/edu/uc/muhammus/stara/ui/main/SearchFragment.kt
+++ app/src/main/java/edu/uc/muhammus/stara/ui/main/SearchFragment.kt
@@ -4,24 +4,25 @@
 package edu.uc.muhammus.stara.ui.main
 
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.uc.muhammus.stara.R
-import edu.uc.muhammus.stara.dto.Show
+import edu.uc.muhammus.stara.dto.ShowJSON
+import edu.uc.muhammus.stara.ui.adapter.ShowsRecyclerViewAdapter
 import kotlinx.android.synthetic.main.search_fragment.*
 
 class SearchFragment : StaraFragment() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var showsRecyclerViewAdapter: ShowsRecyclerViewAdapter
     private var fragmentTitle = "Stara - Search"
+    private var _shows = ArrayList<ShowJSON>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -34,6 +35,26 @@ class SearchFragment : StaraFragment() {
         // Updated deprecated code: https://stackoverflow.com/q/57534730
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        // Configure recycler view options with sane defaults
+        searchRecyclerView.hasFixedSize()
+        searchRecyclerView.layoutManager = LinearLayoutManager(context)
+        searchRecyclerView.itemAnimator = DefaultItemAnimator()
+
+        // Configure recycler view adapters
+        showsRecyclerViewAdapter = ShowsRecyclerViewAdapter(_shows, R.layout.list_item_show)
+
+        // Set default recycler view adapter
+        searchRecyclerView.adapter = showsRecyclerViewAdapter
+
+        viewModel.shows.observe(viewLifecycleOwner, Observer {
+            shows ->
+                // Remove all shows in there
+                _shows.removeAll(_shows)
+                // Update with the new shows that we have observed
+                _shows.addAll(shows)
+                // Tell recycler view to update
+                searchRecyclerView.adapter = showsRecyclerViewAdapter
+        })
         btnSearch.setOnClickListener{populateSearchListView()}
     }
 
@@ -45,15 +66,17 @@ class SearchFragment : StaraFragment() {
             viewModel.fetchShows(searchTerm)
 
             viewModel.shows.observe(viewLifecycleOwner, Observer{
-                shows -> searchListView.adapter = ShowListViewAdapter(requireContext(), shows)
+                shows -> searchRecyclerView.adapter = ShowsRecyclerViewAdapter(shows, R.layout.list_item_show)
             })
+
+            showsRecyclerViewAdapter.notifyDataSetChanged()
         }
         else if (searchRadioActor.isChecked)
         {
             viewModel.fetchActors(searchTerm)
 
             viewModel.actors.observe(viewLifecycleOwner, Observer {
-                actors -> searchListView.adapter = ActorListViewAdapter(requireContext(), actors)
+                //actors -> searchListView.adapter = ActorListViewAdapter(requireContext(), actors)
             })
         }
 
